829c3e6a48c2ee9d52770a42be89a5aa2f243453
Катерина Чакмазова <chakmazova06@gmail.com>
2025-07-04 13:35:21 +0300

    Simplify decision

--- src/algorytm.js
+++ src/algorytm.js
@@ -1,30 +1,14 @@
 'use strict'
-
-const inputs = Array.from(document.getElementsByTagName('input'));
-const buttonCount = document.querySelector('button')
-const answer = document.getElementById('answer');
-
-function turnIntoNumber(str){
-  const match = str.match(/\d+(\.\d+)?/);
-  const number = match ? Math.round(Number(match[0])) : 0;
-  return number;
-}
-
 function findNSD(n1, n2) {
-  let res = n1%n2;
-  return (res===0 ? n2 : findNSD(n2, res))
+  return (n1%n2===0 ? n2 : findNSD(n2, n1%n2))
+}
+const n1 = 34;
+const n2 = 17;
+try {
+  if(Math.round(n1) === 0 || Math.round(n2) === 0) throw new Error ('Number can\'t be a zero')
+  console.log(findNSD(Math.round(n1), Math.round(n2)));
+} catch (error) {
+  console.log(error.message)
 }
 
-buttonCount.addEventListener('click', (event) => {
-  event.preventDefault();
-  let n1, n2;
-  n1 = turnIntoNumber(inputs[0].value);
-  n2 = turnIntoNumber(inputs[1].value);
-  answer.removeAttribute('hidden');
-  if(n1 === 0 || n2 === 0){
-    answer.textContent = 'Please, don\'t enter a 0.'
-  }
-  else {
-    answer.textContent = findNSD(n1, n2);
-  }
-})
+
