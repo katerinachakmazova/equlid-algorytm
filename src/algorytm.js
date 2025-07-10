a81eeebdf04ea135eed018ec33944a31ac773f35
Катерина Чакмазова <chakmazova06@gmail.com>
2025-07-10 12:17:50 +0300

    move try catch into function

--- src/algorytm.js
+++ src/algorytm.js
@@ -1,18 +1,15 @@
 'use strict'
 function findNSD(n1, n2) {
-  return (n1%n2===0 ? n2 : findNSD(n2, n1%n2))
+  try {
+    if(!Number.isInteger(n1) || !Number.isInteger(n2)) throw new Error('Error: numbers must be integers');
+    if(n1 <= 0 || n2 <= 0) throw new Error ('Error: numbers must be bigger than 0')
+    return (n1%n2===0 ? n2 : findNSD(n2, n1%n2))
+  } catch (error) {
+    return error.message;
+  }
 }
-let answer;
-const n1 = 15;
-const n2 = 17;
-
-try {
-if(!Number.isInteger(n1) || !Number.isInteger(n2)) throw new Error('Numbers should be integers');
-if(n1 <= 0 || n2 <= 0) throw new Error ('Numbers should be greater than 0');
-answer = findNSD(n1, n2);
-} catch (error) {
-  answer = error.message;
-}
-console.log(answer)
+const n1 = 280;
+const n2 = 120;
+console.log(findNSD(n1, n2))
 
 
